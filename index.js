4fb6e8343e759b12e2d4e9b5a2aec27811892684
Breedlove13 <b.breedlove@outlook.com>
2021-04-10 13:33:28 -0700

    still unable to get it to restart the questions

--- index.js
+++ index.js
@@ -5,16 +5,7 @@ const empCardGenerator = require('./src/EmployeeGenerator')
 //const engineer =require('')
 
 
-const employeeInfo = (employeeData) => {
-  console.log(`
-  ++++++++++++++++++++
-  New employee Card
-  ++++++++++++++++++++`);
-
-  if (!employeeData) {
-    employeeData = [];
-  }
-
+const generalEmployee = () => {
   return inquirer.prompt([
       {
         type: "input",
@@ -55,146 +46,197 @@ const employeeInfo = (employeeData) => {
       }
     },
     },
+
+    {      
+      type: "list",
+      name: "role",
+      message: "What is the employee's role?",
+      choices: ["Manager", "Engineer", "Intern"],
+    }, 
+   
   ]);
-  
 };
 
-employeeInfo().then((employeeData) => {
-  console.log(employeeData);
-  const employeeCard = empCardGenerator(employeeData);
 
-  fs.writeFile("./index.html", employeeCard, (err) => {
-    if (err) throw new Error(err);
-    console.log("Employee Document Generated");
+const employeeType = (generalEmployeeData) => {
+
+  if (!generalEmployeeData.position) {
+    generalEmployeeData.position = [];
+  }
+
+  return inquirer
+    .prompt([
+
+    {
+         type: "input",
+         name: "officeNumber",
+         message: "What is the Manager's office number?",
+    },
+    {
+        type: "input",
+         name: "github",
+         message: "What is their Github URL?",
+          message: "What is your Github URL?",
+          validate: (githubUrl) => {
+            if (githubUrl) {
+              return true;
+            } else {
+              console.log("Please enter a valid Guthub URL.");
+              return false;
+            }
+          },
+     },   
+     {
+       type: "input",
+       name: "school",
+       message: "What school do they Attend?",
+       validate: (schoolName) => {
+         if (schoolName) {
+           return true;
+          } else {
+            console.log("Please enter the school name.");
+            return false;
+          }
+        },
+      },
+
+      {
+        type: 'confirm',
+        name: 'addEmployee',
+        message: 'Would you like to enter another Employee?',
+        default: false
+      
+      },
+  ])
+  .then ((employeeTypeData) =>{ 
+    generalEmployeeData.position.push(employeeTypeData);
+    if(employeeTypeData.addEmployee) {
+     generalEmployee(generalEmployeeData)
+    }else {
+      return generalEmployeeData
+    }
+    
   });
-});
+
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function employeeInfo(){
-
-//   return inquirer
-//    
-      
-      
-//         type: "list",
-//         name: "role",
-//         message: "What is the employee's role?",
-//         choices: ["Manager", "Engineer", "Intern"],
-//       },
-//     ])
-//     .then((choice) => {
-//       if (choice.role === "Manager") {
-//         infoManager();
-//       } else if (choice.role === "Engineer") {
-//         infoEngineer();
-//       } else { 
-//           infoIntern();
-//       }
-//     });
-// }
-
-
-
-// function infoManager() {
-//   return inquirer.prompt([
-//     {
-//       type: "input",
-//       name: "officeNumber",
-//       message: "What is the Manager's office number?",
-//       validate: (number) => {
-//         if (number) {
-//           return true;
-//         } else {
-//           console.log("Please enter an office number");
-//           return false;
-//         }
-//       },
-//     },
-//   ])//.then(managerData =>  );
-// }
-
-// function infoEngineer() {
-//   return inquirer.prompt([
-//     {
-//       type: "input",
-//       name: "github",
-//       message: "What is their Github URL?",
-//       message: "What is your Github URL?",
-//       validate: (githubUrl) => {
-//         if (githubUrl) {
-//           return true;
-//         } else {
-//           console.log("Please enter a valid Guthub URL.");
-//           return false;
-//         }
-//       },
-//     },
-//   ]);
-// }
-
-// function infoIntern() {
-//   return inquirer.prompt([
-//     {
-//       type: "input",
-//       name: "school",
-//       message: "What school do they Attend?",
-//       validate: (schoolName) => {
-//         if(schoolName){
-//         return true;
-//       } else {
-//         console.log("Please enter the school name.")
-//         return false
-//       }
-//     }
-//    },
-   
-//   ]);
+generalEmployee()
+.then(employeeType)
+.then((employeeTypeData) => {
+  console.log(employeeTypeData)
+  console.log(generalEmployeeData)
+})
   
-// }
+  
+  
+  
+//   console.log(employeeData);
+//   const employeeCard = empCardGenerator(employeeData);
+  
+//   fs.writeFile("./index.html", employeeCard, (err) => {
+//     if (err) throw new Error(err);
+//     console.log("Employee Document Generated");
+//   });
+// });
 
-// function addAnother(){
-//    return inquirer.prompt([
-//      {
-//        type: "confirm",
-//        name: "plusOne",
-//        message: "Would you like to add another employee to your list?",
-//        default: true
-//      }]).then(choice => {
-//          if(choice.plusOne){
-//          employeeInfo()
-//          } else {
-//              console.log('Your list has generated')
-            
-//          }
-//      }); 
-// }
+//   .then((choice) => {
+// if (choice.role === "Manager") {
+//       infoManager();
+      
+//    } else if (choice.role === "Engineer") {
+//       infoEngineer();
+ 
+//    } else { 
+//       infoIntern();
+        
+//     }
+//   });
+//  }
+
+ //console.log(employeeInfo())
+
+    // function infoManager() {
+    //   return inquirer.prompt([
+    //     {
+    //       type: "input",
+    //       name: "officeNumber",
+    //       message: "What is the Manager's office number?",
+    //     },
+    //   ]).then(answer => {
+    //     employeeData.push(answer.name, answer.id, answer.email, answer.officeNumber)
+    //     console.log(answer)
+    //     this.addAnother();
+    //   });; //.then(managerData =>  );
+    // }
+    
+    
+    // function infoEngineer() {
+    //   return inquirer.prompt([
+    //     {
+    //       type: "input",
+    //       name: "github",
+    //       message: "What is their Github URL?",
+    //       message: "What is your Github URL?",
+    //       validate: (githubUrl) => {
+    //         if (githubUrl) {
+    //           return true;
+    //         } else {
+    //           console.log("Please enter a valid Guthub URL.");
+    //           return false;
+    //         }
+    //       },
+    //     },
+    //   ]).then(answer => {
+    //     employeeData.map(infoEngineer (answer.employeeData, answer.github))
+    //     console.log(answer)
+    //     this.addAnother();
+    //   });;
+    // }
+    
+    // function infoIntern() {
+    //   return inquirer.prompt([
+    //     {
+    //       type: "input",
+    //       name: "school",
+    //       message: "What school do they Attend?",
+    //       validate: (schoolName) => {
+    //         if (schoolName) {
+    //           return true;
+    //         } else {
+    //           console.log("Please enter the school name.");
+    //           return false;
+    //         }
+    //       },
+    //     },
+    //   ]) .then(answer => {
+    //     employeeData.map(infoIntern (answer.employeeData, answer.school))
+    //     console.log(answer)
+    //     this.addAnother();
+    //   });
+    // }
+    
+    // function addAnother() {
+    //   return inquirer
+    //     .prompt([
+    //       {
+    //         type: "confirm",
+    //         name: "plusOne",
+    //         message: "Would you like to add another employee to your list?",
+    //         default: true,
+    //       },
+    //     ])
+    //     .then((choice) => {
+    //       if (choice.plusOne) {
+    //         employeeInfo();
+    //       } else {
+    //         console.log("Your list has generated");
+    //       }
+    //     });
+    // }
+    
+    
 
 
-// employeeInfo().then(input =>{
-//   employeeData.push(input)
-// }) 
-//   console.log(employeeData)
 
-// employeeInfo()
-//        .then((employeeData) => {
-//          console.log(employeeData)
-//          const empCards = empcardGenerator(employeeData);
 
-//      fs.writeFile('./index.html', empCards, err => {
-//         if(err) throw new Error(err);
-//         console.log('Employee Cards Generated')
-//      });
-//  })
